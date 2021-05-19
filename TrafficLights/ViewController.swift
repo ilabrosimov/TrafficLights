b8b1a524d63f39bacf7d3d5b7d6f7c5ef047f009
ilabrosimov <ilabrosimov@gmail.com>
2021-05-19 09:52:02 +0300

    Renamed views to redView, yellowView, greenView Added constraints Added code for app

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IB Outlets
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    //MARK: - Constants
+    let RADIUS_OF_TRAFFIC_LIGHT :CGFloat = 40
+    let TURN_OFF_THE_LIGHT :CGFloat = 0.3
+    
+    //MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nextButton.setTitle("START", for: .normal)
+        redView.alpha = TURN_OFF_THE_LIGHT
+        redView.layer.cornerRadius = RADIUS_OF_TRAFFIC_LIGHT
+        yellowView.alpha = TURN_OFF_THE_LIGHT
+        yellowView.layer.cornerRadius = RADIUS_OF_TRAFFIC_LIGHT
+        greenView.alpha = TURN_OFF_THE_LIGHT
+        greenView.layer.cornerRadius = RADIUS_OF_TRAFFIC_LIGHT
     }
 
-
+    //MARK: - IB Actions
+    @IBAction func tappedNextButton(_ sender: UIButton) {
+        if nextButton.titleLabel?.text == "START" {
+            nextButton.setTitle("NEXT", for: .normal)
+            redView.alpha = 1
+        } else {
+            if redView.alpha == 1 {
+                yellowView.alpha = 1
+                redView.alpha = TURN_OFF_THE_LIGHT
+            } else {
+                if yellowView.alpha == 1 {
+                    greenView.alpha = 1
+                    yellowView.alpha = TURN_OFF_THE_LIGHT
+                } else {
+                    if greenView.alpha == 1 {
+                        redView.alpha = 1
+                        greenView.alpha = TURN_OFF_THE_LIGHT
+                    }
+                }
+                
+                
+            }
+            
+            
+        }
+        
+    }
+    
 }
 
